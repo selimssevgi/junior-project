dcd7492ba7d8e8bbb37c6b78c6c464fcce9fbdbe
selimssevgi <selimssevgi@gmail.com>
2016-03-13 15:28:35 +0200

    add guardfile

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+## Added from railstutorial ##
+require 'minitest/reporters'
+Minitest::Reporters.use!
+## Added from railstutorial ##
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
