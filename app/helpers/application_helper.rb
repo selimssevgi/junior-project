df7187c317bc9f4ae3d879d088b9421034ee5303
selimssevgi <selimssevgi@gmail.com>
2016-03-13 16:24:10 +0200

    add full_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Advanced Movie Recommendation System"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
