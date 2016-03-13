8d77bd734d85dadc8be2af7658f9713a7a0bf84d
selimssevgi <selimssevgi@gmail.com>
2016-03-13 15:02:49 +0200

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Advanced Movie Recommendation System"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Advanced Movie Recommendation System"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Advanced Movie Recommendation System"
+  end
 end
