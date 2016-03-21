2d94d75b30427e7256f8991983667bee9f71a90b
selimssevgi <selimssevgi@gmail.com>
2016-03-22 01:46:08 +0200

    some changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,13 @@
 class Movie < ActiveRecord::Base
   belongs_to :rated
+
   has_many   :genremovies
   has_many   :genres, through: :genremovies
 
   has_many   :directings
   has_many   :directors, through: :directings
+ #has_many   :directors, through: :directings, class_name: "Director",
+                                                  #source: :director
 
   has_many   :actings
   has_many   :actors, through: :actings
