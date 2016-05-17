85b3fdd09d40d8e0462865c4214d5eb2bd90b446
selimssevgi <selimssevgi@gmail.com>
2016-05-17 17:29:37 +0300

    improvements and about page

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -3,7 +3,17 @@ class RecommendationsController < ApplicationController
 
   def index
     @movies = current_user.recommended_movies
-    @movies = @movies.where(:show, 1).order('guessed_rate DESC')
+    @movies = @movies.order('guessed_rate DESC')
     @movies = @movies.paginate(page: params[:page]) 
+  end
+
+  def destroy
+    recommended_movie = Recommendation.find(params[:id])
+    @movie_id = recommended_movie.movie_id
+    recommended_movie.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
--- db/migrate/20160517104023_add_show_to_recommendations.rb
+++
@@ -1,5 +0,0 @@
-class AddShowToRecommendations < ActiveRecord::Migration
-  def change
-    add_column :recommendations, :show, :boolean, default: true
-  end
-end
